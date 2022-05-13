623bec30fb065e4e16991531bd64aaf80b1c6420
masud rana <masudranaacb@gmail.com>
2022-05-14 00:05:18 +0600

    basic sever setup and all service added

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000
+
+// mideltwer
+app.use(cors())
+app.use(express.json())
+
+// collection information
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1kyfa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection=client.db('doctors_portal').collection('services')
+        console.log('db connected')
+        app.get('/service',async(req,res)=>{
+            const query={}
+            const cursor = serviceCollection.find(query);
+            const services=await cursor.toArray();
+            res.send(services)
+        })
+    } finally {
+        
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('hello Doctors portal')
+})
+
+app.listen(port, () => {
+    console.log(`Doctors potal  app listening on port ${port}`)
+})
