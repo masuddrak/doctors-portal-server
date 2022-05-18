ae43d35e53c6a02e69382094b73758de6d111b32
masud rana <masudranaacb@gmail.com>
2022-05-18 11:52:11 +0600

    jwt token verify

--- index.js
+++ index.js
@@ -13,55 +13,73 @@ app.use(express.json())
 // collection information
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1kyfa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+function accessJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unAuthorization Access' })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Authorization Access Error' })
+        }
+        req.decoded = decoded
+        next()
+    });
+}
 
 async function run() {
     try {
         await client.connect();
-        const serviceCollection=client.db('doctors_portal').collection('services')
-        const bookingCollection=client.db('doctors_portal').collection('bookis')
-        const userCollection=client.db('doctors_portal').collection('users')
+        const serviceCollection = client.db('doctors_portal').collection('services')
+        const bookingCollection = client.db('doctors_portal').collection('bookis')
+        const userCollection = client.db('doctors_portal').collection('users')
 
-        app.get('/service',async(req,res)=>{
-            const query={}
+        app.get('/service', async (req, res) => {
+            const query = {}
             const cursor = serviceCollection.find(query);
-            const services=await cursor.toArray();
+            const services = await cursor.toArray();
             res.send(services)
         })
-      
+
 
         // bookings find query system
-        app.get('/booking', async(req,res)=>{
-            const patient=req.query.patientee
-            const query={patient:patient}
-            const bookings= await bookingCollection.find(query).toArray();
-            res.send(bookings)
-        })
-        app.post('/booking',async(req,res)=>{
-            const booking=req.body;
-            const query={treatmentName:booking.treatmentName, date:booking.date, patient:booking.patient}
-            const exists= await bookingCollection.findOne(query);
-            if(exists){
-                return res.send({success:false, booking:exists})
+        app.get('/booking', accessJWT, async (req, res) => {
+            const patient = req.query.patientee
+            const decodedEmail = req.decoded.email
+            if (decodedEmail === patient) {
+                const query = { patient: patient }
+                const bookings = await bookingCollection.find(query).toArray();
+                return res.send(bookings)
+            } else {
+                return res.status(403).send({ message: 'Forbiden Authorization Access' })
             }
-            const result=await bookingCollection.insertOne(booking)
-            return res.send({success:true,result})
+        })
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = { treatmentName: booking.treatmentName, date: booking.date, patient: booking.patient }
+            const exists = await bookingCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, booking: exists })
+            }
+            const result = await bookingCollection.insertOne(booking)
+            return res.send({ success: true, result })
         })
         // update user
-        app.put('/user/:email',async(req,res)=>{
-            const email=req.params.email
-            const user=req.body
-            const filter= {email:email}
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email
+            const user = req.body
+            const filter = { email: email }
             const options = { upsert: true };
             const updateDoc = {
                 $set: user,
-              };
-              const result = await userCollection.updateOne(filter, updateDoc, options);
-              const token=jwt.sign({ email: email },process.env.ACCESS_TOKEN,{ expiresIn: '1h' });
-              res.send({result,token})
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' });
+            res.send({ result, token })
         })
     } finally {
-        
+
     }
 }
 run().catch(console.dir);
